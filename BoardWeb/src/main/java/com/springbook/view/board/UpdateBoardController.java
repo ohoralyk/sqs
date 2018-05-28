6e42565028c7997c343ff54ef3d00c91d8467b28
ohoralyk <ohoralyk@gmail.com>
2018-05-28 17:25:39 +0900

    4-2-5

--- BoardWeb/src/main/java/com/springbook/view/board/UpdateBoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/UpdateBoardController.java
@@ -1,36 +1,20 @@
 package com.springbook.view.board;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
-import com.springbook.view.controller.Controller;
 
-public class UpdateBoardController implements Controller {
+@Controller
+public class UpdateBoardController {
 
-	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping("/updateBoard.do")
+	public String updateBoard(BoardVO vo, BoardDAO boardDAO) {
 		System.out.println("글 수정 처리");
-		
-		//1. 사용자 입력 정보 추출
-		//request.setCharacterEncoding("EUC-KR");
-		String title 	= request.getParameter("title");
-		String content 	= request.getParameter("content");
-		String seq		= request.getParameter("seq");
-		
-		//2. DB 연동 처리
-		BoardVO vo = new BoardVO();
-		
-		vo.setTitle(title);
-		vo.setContent(content);
-		vo.setSeq(Integer.parseInt(seq));
-		
-		BoardDAO boardDAO = new BoardDAO();
-		
+
 		boardDAO.updateBoard(vo);
 		
-		//3. 화면 네비게이션
 		return "getBoardList.do";
 	}
 
